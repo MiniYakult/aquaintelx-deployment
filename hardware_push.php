a6cb555536806bc7259e436df1c86606259db4b7
charlesbarandoc <barandoccharles13@gmail.com>
2026-06-30 18:59:46 +0800

    Test root hardware push endpoint

--- hardware_push.php
+++ hardware_push.php
@@ -4,6 +4,6 @@ header("Content-Type: application/json");
 echo json_encode([
     "status" => "success",
     "version" => "hardware_push_test_v1",
-    "message" => "hardware_push.php is updated and running"
+    "message" => "This is the real root hardware_push.php"
 ]);
 ?>
